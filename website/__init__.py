09d17736f922ce455e5296534ccee14c8ff8a793
Donnovan Jiles <djiles@mail.sfsu.edu>
2023-05-02 12:14:48 -0700

    User Authentication and Security

--- website/__init__.py
+++ website/__init__.py
@@ -3,15 +3,41 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "notagoodsecret"
+    # tell flask where database is and initialize the DB with the flask app
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
+    from .models import User
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    create_DB(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    # this function allows us to access info related to the user from the DB
+    # given the id of a user
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+def create_DB(app):
+    # check is path exist and if it doesnt we create it and make the DB file for us
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created database')
--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,68 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email_data = request.form.get('email')
+        password_data = request.form.get('password')
+
+        # check if user exist in DB and if passwords match
+        user_exists = User.query.filter_by(email=email_data).first()
+        if user_exists:
+            if check_password_hash(user_exists.password, password_data):
+                flash('You are logged in!', category='success')
+                login_user(user_exists, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Your password is incorrect.', category='error')
+        else:
+            flash('Your email does not exist.', category='error')
     return render_template('login.html')
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email_data = request.form.get('email')
+        username_data = request.form.get('username')
+        password_1_data = request.form.get('password_1')
+        password_2_data = request.form.get('password_2')
+
+        # check if user already exists and if passwords are valid
+        # todo verify email
+        email_exists = User.query.filter_by(email=email_data).first()
+        username_exist = User.query.filter_by(username=username_data).first()
+        if email_exists:
+            flash('This email is already in use.', category='error')
+        elif username_exist:
+            flash('This username is already in use.', category='error')
+        elif password_1_data != password_2_data:
+            flash('Passwords don\'t match!', category='error')
+        elif len(username_data) < 4:
+            flash('Username is too short', category='error')
+        elif len(password_1_data) < 6:
+            flash('Password is too short', category='error')
+        else:
+            new_user = User(email=email_data,
+                            username=username_data, password=generate_password_hash(password_1_data, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('User created')
+            return redirect(url_for('views.home'))
+
     return render_template('signup.html')
 
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
